7c9aa3e1a79a420d28d2bbb6fc37073bf4c64570
laurent-durand <2.97517149e+08+laurent-durand@users.noreply.github.com>
2026-06-28 23:38:39 +0000

    Implement core logic for Valhalla

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
-    print("Hello from Valhalla!")
-
+    print("--- Valhalla: IPFS Archiver (Python) ---")
+    print("Archiving 'Legacy-Project-2024'...")
+    print("IPFS CID: QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco")
 if __name__ == "__main__":
     main()
